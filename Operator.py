3fcbc60d23d1f215a125c2a40a30c54f4a31ced9
Akash Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-11 22:11:55 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,41 @@
+# Arithmetic operators
+a = 10
+b = 3
+print("Addition:", a + b)       # Addition
+print("Subtraction:", a - b)   # Subtraction
+print("Multiplication:", a * b) # Multiplication
+print("Division:", a / b)      # Division (float result)
+print("Floor Division:", a // b) # Floor Division (integer result)
+print("Modulus:", a % b)       # Modulus
+print("Exponentiation:", a ** b) # Exponentiation
+
+# Comparison operators
+print("Equal:", a == b)        # Equal
+print("Not Equal:", a != b)    # Not equal
+print("Greater Than:", a > b)  # Greater than
+print("Less Than:", a < b)     # Less than
+print("Greater or Equal:", a >= b) # Greater or equal
+print("Less or Equal:", a <= b)    # Less or equal
+
+# Logical operators
+x = True
+y = False
+print("Logical AND:", x and y) # AND
+print("Logical OR:", x or y)  # OR
+print("Logical NOT:", not x)  # NOT
+
+# Assignment operators
+num = 5
+print("Initial value:", num)
+num += 3  # Equivalent to num = num + 3
+print("After += 3:", num)
+num -= 2  # Equivalent to num = num - 2
+print("After -= 2:", num)
+num *= 4  # Equivalent to num = num * 4
+print("After *= 4:", num)
+num /= 3  # Equivalent to num = num / 3
+print("After /= 3:", num)
+num %= 3  # Equivalent to num = num % 3
+print("After %= 3:", num)
+num **= 2  # Equivalent to num = num ** 2
+print("After **= 2:", num)
